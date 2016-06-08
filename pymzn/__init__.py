289c2c74bf133a410c94b414834fd8d0ed35a286
paolodragone <dragone.paolo@gmail.com>
2016-06-08 16:21:42 +0200

    Updated version.

--- pymzn/__init__.py
+++ pymzn/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Wrapper module for the MiniZinc tool pipeline."""
-__version__ = '0.9.3'
+__version__ = '0.9.4'
 
 from .mzn import fzn_gecode, minizinc, mzn2fzn, solns2out, MiniZincError, run
 from .dzn import dzn, parse_dzn, dict2array, MiniZincParsingError
