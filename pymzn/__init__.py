a1da833bab4d93739f9223e918c386bfb52b3c97
paolodragone <paolo.dragone@unitn.it>
2016-07-25 18:09:22 +0200

    Updated version.

--- pymzn/__init__.py
+++ pymzn/__init__.py
@@ -14,7 +14,7 @@ from ._dzn import *
 from . import _mzn
 from ._mzn import *
 
-__version__ = '0.10.4'
+__version__ = '0.10.5'
 __all__ = ['debug', 'config', 'bin']
 __all__.extend(_dzn.__all__)
 __all__.extend(_mzn.__all__)
