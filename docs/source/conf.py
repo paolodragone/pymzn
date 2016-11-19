46ab09cc99350f98c82e27bb5aaafcde7488b3b5
Paolo Dragone <dragone.paolo@gmail.com>
2016-11-19 12:50:04 +0100

    docs: remove import of package, find version as in setup.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
 import os
+import re
+import sys
+import codecs
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def find_version(*parts):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *parts), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 sys.path.insert(0, os.path.abspath('../../'))
 
@@ -33,7 +52,6 @@ project = 'PyMzn'
 copyright = '2016, Paolo Dragone (MIT Licence)'
 author = 'Paolo Dragone'
 
-import pymzn
-version = pymzn.__version__
-release = pymzn.__version__
+version = find_version('..', '..', 'pymzn', '__init__.py')
+release = find_version('..', '..', 'pymzn', '__init__.py')
 
