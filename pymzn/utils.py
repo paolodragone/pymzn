b37379c6004ead8032679c4d8ced0438771d2657
Paolo Dragone <dragone.paolo@gmail.com>
2017-06-04 15:11:34 +0200

    utils: move run utility to utils

--- pymzn/utils.py
+++ pymzn/utils.py
@@ -1,5 +1,6 @@
 
 import logging
+import subprocess
 
 
 """ Logging utilities """
@@ -25,4 +26,45 @@ class BracesAdapter(logging.LoggerAdapter):
 
 def get_logger(name):
     return BracesAdapter(logging.getLogger(name))
+
+
+
+""" Binary utilities """
+
+def run(args, stdin=None):
+    """Executes a command and waits for the result.
+
+    Parameters
+    ----------
+    args : list
+        The list of arguments for the program to execute. Arguments should be
+        formatted as for the ``subprocess.Popen`` constructor.
+    stdin : str or bytes
+        String or bytes containing the input stream for the process.
+
+    Returns
+    -------
+    CompletedProcess
+        An instance of CompletedProcess containing the information about
+        the executed process, including stdout, stderr and running time.
+
+    Raises
+    ------
+    CalledProcessError
+        When the process returns an error.
+    """
+    log = get_logger(__name__)
+    log.debug('Executing command with args: {}', args)
+    start = time.time()
+    with subprocess.Popen(args, bufsize=1, universal_newlines=True,
+                          stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE) as process:
+        out, err = process.communicate(stdin)
+        ret = process.poll()
+    elapsed = time.time() - start
+    log.debug('Done. Running time: {0:.2f} seconds'.format(elapsed))
+    process = subprocess.CompletedProcess(args, ret, out, err)
+    process.check_returncode()
+    return process
 
