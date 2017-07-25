c1c48d7def3139ef5174c483dd71b154a47ce31c
Paolo Dragone <dragone.paolo@gmail.com>
2017-07-25 11:55:34 +0200

    mzn: add new solvers imports

--- pymzn/mzn/__init__.py
+++ pymzn/mzn/__init__.py
@@ -8,5 +8,7 @@ __all__ = ['SolnStream', 'minizinc', 'mzn2fzn', 'solns2out', 'MiniZincError',
            'MiniZincUnsatisfiableError', 'MiniZincUnknownError',
            'MiniZincUnboundedError', 'MiniZincModel', 'Statement', 'Constraint',
            'Variable', 'ArrayVariable', 'OutputStatement', 'SolveStatement',
-           'Solver', 'Gecode', 'Optimathsat', 'Opturion', 'Gurobi', 'gecode',
-           'optimathsat', 'opturion', 'gurobi']
+           'Solver', 'Gecode', 'Chuffed', 'Optimathsat', 'Opturion',
+           'MIPSolver', 'Gurobi', 'CBC', 'G12Solver', 'G12Fd', 'G12Lazy',
+           'G12MIP', 'gecode', 'chuffed', 'optimathsat', 'opturion', 'gurobi',
+           'cbc', 'g12fd', 'g12lazy', 'g12mip']
