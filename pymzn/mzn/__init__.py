a529d46fbec99c3b8b4b5a65603706ca194fbc14
Paolo Dragone <dragone.paolo@gmail.com>
2017-08-06 14:35:58 +0200

    mzn: fix imports

--- pymzn/mzn/__init__.py
+++ pymzn/mzn/__init__.py
@@ -5,10 +5,10 @@ from .solvers import *
 from .minizinc import *
 
 __all__ = ['SolnStream', 'minizinc', 'mzn2fzn', 'solns2out', 'MiniZincError',
-           'MiniZincUnsatisfiableError', 'MiniZincUnknownError',
-           'MiniZincUnboundedError', 'MiniZincModel', 'Statement', 'Constraint',
-           'Variable', 'ArrayVariable', 'OutputStatement', 'SolveStatement',
-           'Solver', 'Gecode', 'Chuffed', 'Optimathsat', 'Opturion',
-           'MIPSolver', 'Gurobi', 'CBC', 'G12Solver', 'G12Fd', 'G12Lazy',
-           'G12MIP', 'OscarCBLS', 'gecode', 'chuffed', 'optimathsat', 'opturion', 'gurobi',
-           'cbc', 'g12fd', 'g12lazy', 'g12mip', 'oscar_cbls']
+        'MiniZincUnsatisfiableError', 'MiniZincUnknownError',
+        'MiniZincUnboundedError', 'MiniZincModel', 'Statement', 'Constraint',
+        'Variable', 'ArrayVariable', 'OutputStatement', 'SolveStatement',
+        'Solver', 'Gecode', 'Chuffed', 'Optimathsat', 'Opturion', 'MIPSolver',
+        'Gurobi', 'CBC', 'G12Solver', 'G12Fd', 'G12Lazy', 'G12MIP', 'OscarCBLS',
+        'gecode', 'chuffed', 'optimathsat', 'opturion', 'gurobi', 'cbc',
+        'g12fd', 'g12lazy', 'g12mip', 'oscar_cbls']
