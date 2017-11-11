025b62a954ebc781c0fce273b2d419b2c73976e4
Paolo Dragone <dragone.paolo@gmail.com>
2017-11-11 17:56:38 +0100

    templates: refactoring + add docstrings

--- pymzn/mzn/templates.py
+++ pymzn/mzn/templates.py
@@ -1,3 +1,63 @@
+"""Handles MiniZinc template files.
+
+PyMzn supports templating as a form of dynamic modelling. PyMzn allows to embed
+code from the `Jinja2 <http://jinja.pocoo.org/>`_ templating language within a
+MiniZinc model to make a PyMzn template file (usually distinguished with the
+`pmzn` extension). An example::
+
+    %% knapsack.pmzn %%
+    int: n;                     % number of objects
+    set of int: OBJ = 1..n;
+    int: capacity;              % the capacity of the knapsack
+    array[OBJ] of int: size;    % the size of each object=
+
+    var set of OBJ: x;
+    constraint sum(i in x)(size[i]) <= capacity;
+
+    {% if objective == 'profit' %}
+        array[OBJ] of int: profit;  % the profit of each object
+        solve maximize sum(i in x)(profit[i]);
+    {% elif objective == 'cost' %}
+        array[OBJ] of int: cost;    % the cost of each object
+        solve minimize sum(i in x)(cost[i]);
+    {% else %}
+        solve satisfy;
+    {% endif%}
+
+    %% knapsack.dzn %%
+    n = 5;
+    size = [14, 4, 10, 6, 9];
+    capacity = 20;
+
+Now it is possible to pass `objective` as a member of `args` in the
+`pymzn.minizinc` function::
+
+    pymzn.minizinc('knapsack.pmzn', data={'cost': [10, 3, 9, 4, 8]}, args={'objective': 'cost'})
+
+The compiled model that will be solved looks like this::
+
+    %% knapsack.mzn %%
+    int: n;                     % number of objects
+    set of int: OBJ = 1..n;
+    int: capacity;              % the capacity of the knapsack
+    array[OBJ] of int: size;    % the size of each object=
+
+    var set of OBJ: x;
+    constraint sum(i in x)(size[i]) <= capacity;
+
+    array[OBJ] of int: cost;    % the cost of each object
+    solve minimize sum(i in x)(cost[i]);
+
+Notice that now the model expects a `cost` array as dzn input data, so we passed
+it with the `data` argument as usual.
+
+As mentioned, PyMzn employs Jinja2 under the hood, so anything you can do with
+Jinja2 is also possible in PyMzn, including variables, control structured,
+template inheritance, and filters. PyMzn implements few custom filters as well:
+- `int(value, factor=100)` : discretizes the given input or array,
+  pre-multiplying by the given factor.
+- `dzn(value)` : transform the input into its equivalent dzn string.
+"""
 
 from copy import deepcopy
 from collections.abc import Iterable
@@ -8,7 +68,11 @@ from jinja2 import (
 )
 
 
+__all__ = ['discretize', 'from_string', 'add_package', 'add_path']
+
+
 def discretize(value, factor=100):
+    """Discretize the given value, pre-multiplying by the given factor"""
     if not isinstance(value, Iterable):
         return int(value * factor)
     int_value = list(deepcopy(value))
@@ -51,13 +115,16 @@ _jenv.filters['int'] = discretize
 
 
 def from_string(source, args=None):
+    """Renders a template string"""
     return _jenv.from_string(source).render(args or {})
 
 
 def add_package(package_name, package_path='templates', encoding='utf-8'):
+    """Adds the given package to the template search routine"""
     _jload.add_loader(PackageLoader(package_name, package_path, encoding))
 
 
 def add_path(searchpath, encoding='utf-8', followlinks=False):
+    """Adds the given path to the template search routine"""
     _jload.add_loader(FileSystemLoader(searchpath, encoding, followlinks))
 
