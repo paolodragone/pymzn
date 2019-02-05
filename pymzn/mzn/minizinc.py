9e1747b5f8dca30387e061467a67afc36a6f5859
Paolo Dragone <dragone.paolo@gmail.com>
2019-02-05 13:28:25 +0100

    minizinc: factor out exceptions

--- pymzn/mzn/minizinc.py
+++ pymzn/mzn/minizinc.py
@@ -37,6 +37,7 @@ from .solvers import gecode
 from .model import MiniZincModel
 from ..process import Process
 from ..dzn import dict2dzn, dzn2dict
+from ..exceptions import *
 
 
 def minizinc(
@@ -440,66 +441,4 @@ def _to_dict(stream):
             yield dzn2dict(next(stream))
     except StopIteration as stop:
         return stop.value
-
-
-class MiniZincError(RuntimeError):
-    """Generic error for the MiniZinc functions."""
-
-    def __init__(self, msg=None):
-        super().__init__(msg)
-        self._mzn_file = None
-        self._stderr = None
-
-    @property
-    def stderr(self):
-        return self._stderr
-
-    @property
-    def mzn_file(self):
-        """str: the mzn file that generated the error."""
-        return self._mzn_file
-
-    def _set(self, _mzn_file, _stderr):
-        self._mzn_file = _mzn_file
-        self._stderr = _stderr
-        self.args = ('{}: {}'.format(
-            self._mzn_file, self.args[0] + '\n{}'.format(_stderr)
-        ),)
-
-
-class MiniZincUnsatisfiableError(MiniZincError):
-    """Error raised when a minizinc problem is found to be unsatisfiable."""
-
-    def __init__(self):
-        super().__init__('The problem is unsatisfiable.')
-
-
-class MiniZincUnknownError(MiniZincError):
-    """Error raised when minizinc returns no solution (unknown)."""
-
-    def __init__(self):
-        super().__init__('The solution of the problem is unknown.')
-
-
-class MiniZincUnboundedError(MiniZincError):
-    """Error raised when a minizinc problem is found to be unbounded."""
-
-    def __init__(self):
-        super().__init__('The problem is unbounded.')
-
-
-class MiniZincUnsatOrUnboundedError(MiniZincError):
-    """Error raised when a minizinc problem is found to be unsatisfiable or
-    unbounded.
-    """
-
-    def __init__(self):
-        super().__init__('The problem is unsatisfiable or unbounded.')
-
-
-class MiniZincGenericError(MiniZincError):
-    """Error raised when an error occurs but it is none of the above."""
-
-    def __init__(self):
-        super().__init__('The problem raised an error.')
 
