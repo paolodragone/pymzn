4bac664e9b9725c47b2826f30713b8160e83233a
Paolo Dragone <dragone.paolo@gmail.com>
2019-02-05 15:30:23 +0100

    process: add new synchronous subprocess functions

--- pymzn/process.py
+++ pymzn/process.py
@@ -10,7 +10,7 @@ from time import monotonic as _time
 from subprocess import Popen, PIPE, TimeoutExpired, CalledProcessError
 
 
-__all__ = ['Process']
+__all__ = ['Process', 'run']
 
 
 class Process:
@@ -118,4 +118,47 @@ class Process:
                     stdout, stderr = self.stdout_data, self.stderr_data
                     raise CalledProcessError(retcode, self.args, stdout, stderr)
         return self
+
+
+class CompletedProcessWrapper:
+
+    def __init__(self, proc, start_time, end_time):
+        self._proc = proc
+        self.start_time = start_time
+        self.end_time = end_time
+
+    def __repr__(self):
+        return repr(self._proc)
+
+    @property
+    def args(self):
+        return self._proc.args
+
+    @property
+    def returncode(self):
+        return self._proc.returncode
+
+    @property
+    def stdout_data(self):
+        return self._proc.stdout
+
+    @property
+    def stderr_data(self):
+        return self._proc.stderr
+
+
+def run_process(*args, input=None):
+    shell = os.name == 'nt'
+    start_time = _time()
+    cp = subprocess.run(
+        args, input=input, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        shell=shell, bufsize=1, universal_newlines=True
+    )
+    end_time = _time()
+    return CompletedProcessWrapper(cp, start_time, end_time)
+
+
+def run(*args, input=None):
+    proc = run_process(*args, input=input)
+    return proc.stdout_data.decode('utf-8')
 
