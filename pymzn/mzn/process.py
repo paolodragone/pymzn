1b1dfc1c3e2c1f0d2a88cad19506b1988b077ae3
Paolo Dragone <dragone.paolo@gmail.com>
2019-02-07 10:44:34 +0100

    process: add Stream inner class

--- pymzn/mzn/process.py
+++ pymzn/mzn/process.py
@@ -14,8 +14,22 @@ __all__ = ['run_process']
 
 class CompletedProcessWrapper:
 
+    def Stream:
+
+        def __init__(self, proc, transport=1):
+            self._proc = proc
+            self._transport = transport
+
+        def readlines(self):
+            if self._transport == 1:
+                yield from self._proc.stdout_data.splitlines()
+            else:
+                yield from self._proc.stderr_data.splitlines()
+
     def __init__(self, proc, start_time, end_time):
         self._proc = proc
+        self.stdout_stream = CompletedProcessWrapper.Stream(self, 1)
+        self.stderr_stream = CompletedProcessWrapper.Stream(self, 2)
         self.start_time = start_time
         self.end_time = end_time
 
@@ -38,8 +52,13 @@ class CompletedProcessWrapper:
     def stderr_data(self):
         return self._proc.stderr
 
-    def readlines(self):
-        yield from self.stdout_data.splitlines()
+    @property
+    def stdout_data(self):
+        return self.stdout_stream
+
+    @property
+    def stderr_data(self):
+        return self.stderr_stream
 
 
 def run_process(*args, input=None):
