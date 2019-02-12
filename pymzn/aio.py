69798a4bb41aaa3d2549980f0df41f7f06836942
Paolo Dragone <dragone.paolo@gmail.com>
2019-02-12 10:19:27 +0100

    aio: catch syntax errors

--- pymzn/aio.py
+++ pymzn/aio.py
@@ -1,3 +1,8 @@
 
-from .mzn.aio import *
+try:
+    from .mzn.aio import *
+except SyntaxError as err:
+    raise ImportError(
+        'You need Python 3.6 or higher to use the pymzn.aio package.'
+    ) from err
 
