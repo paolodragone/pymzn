7326158e8ff54903996cba855e30973753fe8dcb
Paolo Dragone <dragone.paolo@gmail.com>
2019-02-12 11:01:00 +0100

    aio: add __all__

--- pymzn/aio.py
+++ pymzn/aio.py
@@ -1,6 +1,8 @@
 
 try:
+    from .mzn import aio as _aio
     from .mzn.aio import *
+    __all__ = _aio.__all__
 except SyntaxError as err:
     raise ImportError(
         'You need Python 3.6 or higher to use the pymzn.aio package.'
