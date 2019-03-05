060258bf6f4154dc885c918272ce5e6c691a32a1
Paolo Dragone <dragone.paolo@gmail.com>
2019-03-05 15:54:40 +0100

    examples/asyncronous: small fix

--- examples/asyncronous/async_test.py
+++ examples/asyncronous/async_test.py
@@ -6,7 +6,7 @@ from pymzn.aio import minizinc
 
 async def main():
     solns = await minizinc('async.mzn', all_solutions=True, keep_solutions=False)
-    while solns.status == pymzn.Status.INCOMPLETE:
+    while solns.status is not pymzn.Status.COMPLETE:
         await asyncio.sleep(1)
         for i, soln in enumerate(solns):
             if i == 0:
