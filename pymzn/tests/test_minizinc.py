c77e85ad6ea7d574edf320519d8006e2d1c12cad
Paolo Dragone <dragone.paolo@gmail.com>
2019-03-07 11:19:49 +0100

    tests: fix minizinc test

--- pymzn/tests/test_minizinc.py
+++ pymzn/tests/test_minizinc.py
@@ -1,11 +1,20 @@
 import re
 import os
-import pymzn
-import os.path
 import unittest
-from tempfile import NamedTemporaryFile
 
 from textwrap import dedent
+from tempfile import NamedTemporaryFile
+
+from pymzn import minizinc, mzn2fzn, gecode, cbc, MiniZincError
+
+
+def _save_as_temp_file(content, suffix):
+    with NamedTemporaryFile(
+        prefix='pymzn_', suffix=suffix, mode='w', delete=False
+    ) as f:
+        f.write(content)
+        file_name = f.name
+    return file_name
 
 
 class MinizincTest(unittest.TestCase):
@@ -28,196 +37,186 @@ class MinizincTest(unittest.TestCase):
         profit = [10, 3, 9, 4, 8];
         size = [14, 4, 10, 6, 9];
         ''')
+
     data2 = 'capacity = 20;'
+    data_dict_2 = {'capacity': 20}
+
     data3 = '\n'.join([data1, data2])
+    data_dict_3 = {
+        'N': 5, 'profit': [10, 3, 9, 4, 8], 'size': [14, 4, 10, 6, 9],
+        'capacity': 20
+    }
 
     def test_mzn2fzn(self):
 
-        self.assertRaises(TypeError, pymzn.mzn2fzn, 0)
-        self.assertRaises(TypeError, pymzn.mzn2fzn, {1, 2, 3})
+        # Check that TypeError is raised if anything but a string
+        # is supplied to the mzn2fzn function
+        self.assertRaises(TypeError, mzn2fzn, 0)
+        self.assertRaises(TypeError, mzn2fzn, {1, 2, 3})
 
-        mzn_file = NamedTemporaryFile(prefix='pymzn_', suffix='.mzn', mode='w',
-                                      delete=False)
-        mzn_file.write(self.model)
-        mzn_file.close()
-        mzn = mzn_file.name
-        mzn_base = os.path.splitext(mzn)[0]
+        # Use temporary files for model and data
+        mzn_file = _save_as_temp_file(self.model, '.mzn')
+        dzn_file_1 = _save_as_temp_file(self.data1, '.dzn')
+        dzn_file_2 = _save_as_temp_file(self.data2, '.dzn')
+        dzn_file_3 = _save_as_temp_file(self.data3, '.dzn')
 
-        self.assertRaises(RuntimeError, pymzn.mzn2fzn, mzn)
+        # Check that MiniZincError is raised if no data is supplied
+        self.assertRaises(MiniZincError, mzn2fzn, mzn_file)
 
-        dzn_file = NamedTemporaryFile(prefix='pymzn_', suffix='.dzn', mode='w',
-                                      buffering=1)
-        dzn_file.write(self.data1)
-        dzn = dzn_file.name
-        self.assertRaises(RuntimeError, pymzn.mzn2fzn, mzn, dzn)
-        dzn_file.close()
+        # Check that MiniZincError is raised if not all data needed is supplied
+        self.assertRaises(MiniZincError, mzn2fzn, mzn_file, dzn_file_1)
+        self.assertRaises(MiniZincError, mzn2fzn, mzn_file, dzn_file_2)
 
-        dzn_file = NamedTemporaryFile(prefix='pymzn_', suffix='.dzn', mode='w',
-                                      buffering=1)
-        dzn_file.write(self.data2)
-        dzn = dzn_file.name
-        self.assertRaises(RuntimeError, pymzn.mzn2fzn, mzn, dzn)
-        dzn_file.close()
+        # Check that files returned by mzn2fzn exist and they are not empty
+        fzn_file, ozn_file = mzn2fzn(
+            mzn_file, dzn_file_1, data=self.data_dict_2
+        )
+        self.assertTrue(os.path.isfile(fzn_file))
+        self.assertTrue(os.path.isfile(ozn_file))
 
-        dzn_file = NamedTemporaryFile(prefix='pymzn_', suffix='.dzn', mode='w',
-                                      buffering=1)
-        dzn_file.write(self.data1)
-        dzn_file.flush()
-        dzn = dzn_file.name
-        fzn = mzn_base + '.fzn'
-        ozn = mzn_base + '.ozn'
-        self.assertEqual(pymzn.mzn2fzn(mzn, dzn, data={'capacity': 20}),
-                         (fzn, ozn))
-        dzn_file.close()
-        self.assertTrue(os.path.isfile(fzn))
-        self.assertTrue(os.path.isfile(ozn))
-        os.remove(fzn)
-        os.remove(ozn)
+        fzn_base = os.path.splitext(fzn_file)[0]
+        mzn_file_tmp = fzn_base + '.mzn'
+        self.assertTrue(os.path.isfile(mzn_file_tmp))
 
-        dzn_file = NamedTemporaryFile(prefix='pymzn_', suffix='.dzn', mode='w',
-                                      buffering=1)
-        dzn_file.write(self.data3)
-        dzn_file.flush()
-        dzn = dzn_file.name
-        fzn = mzn_base + '.fzn'
-        ozn = mzn_base + '.ozn'
-        self.assertEqual(pymzn.mzn2fzn(mzn, dzn), (fzn, ozn))
-        dzn_file.close()
-        self.assertTrue(os.path.isfile(fzn))
-        self.assertTrue(os.path.isfile(ozn))
-        os.remove(fzn)
-        os.remove(ozn)
+        with open(mzn_file_tmp) as f:
+            self.assertNotEqual(f.read(), '')
+        with open(fzn_file) as f:
+            self.assertNotEqual(f.read(), '')
+        with open(ozn_file) as f:
+            self.assertNotEqual(f.read(), '')
 
-        dzn_file = NamedTemporaryFile(prefix='pymzn_', suffix='.dzn', mode='w',
-                                      buffering=1)
-        dzn_file.write(self.data3)
-        dzn = dzn_file.name
-        fzn = mzn_base + '.fzn'
-        ozn = mzn_base + '.ozn'
-        self.assertEqual(pymzn.mzn2fzn(mzn, dzn, no_ozn=True), (fzn, None))
-        dzn_file.close()
-        self.assertTrue(os.path.isfile(fzn))
-        self.assertFalse(os.path.isfile(ozn))
-        os.remove(fzn)
+        os.remove(mzn_file_tmp)
+        os.remove(fzn_file)
+        os.remove(ozn_file)
 
-        fzn, ozn = pymzn.mzn2fzn(mzn, data={'N': 5, 'profit': [10, 3, 9, 4, 8],
-                                 'size': [14, 4, 10, 6, 9], 'capacity': 20},
-                                 keep_data=True)
-        dzn = mzn_base + '_data.dzn'
-        self.assertEqual(fzn, mzn_base + '.fzn')
-        self.assertEqual(ozn, mzn_base + '.ozn')
-        self.assertTrue(os.path.isfile(dzn))
-        os.remove(fzn)
-        os.remove(ozn)
-        os.remove(dzn)
+        # Check that no_ozn works
+        fzn_file, ozn_file = mzn2fzn(mzn_file, dzn_file_3, no_ozn=True)
+        self.assertTrue(os.path.isfile(fzn_file))
 
-        os.remove(mzn)
+        fzn_base = os.path.splitext(fzn_file)[0]
+        mzn_file_tmp = fzn_base + '.mzn'
+        self.assertTrue(os.path.isfile(mzn_file_tmp))
 
+        self.assertEqual(ozn_file, None)
+        os.remove(mzn_file_tmp)
+        os.remove(fzn_file)
+
+        # Check that keep works
+        fzn_file, ozn_file = mzn2fzn(
+            mzn_file, data=self.data_dict_3, keep=True
+        )
+        fzn_base = os.path.splitext(fzn_file)[0]
+        mzn_file_tmp = fzn_base + '.mzn'
+        dzn_file = fzn_base + '_data.dzn'
+        self.assertTrue(os.path.isfile(mzn_file_tmp))
+        self.assertTrue(os.path.isfile(dzn_file))
+
+        with open(dzn_file) as f:
+            self.assertNotEqual(f.read(), '')
+
+        os.remove(mzn_file_tmp)
+        os.remove(fzn_file)
+        os.remove(ozn_file)
+        os.remove(dzn_file)
+
+        os.remove(mzn_file)
 
     def test_minizinc(self):
-        out = pymzn.minizinc(self.model,
-                             data={'N': 5, 'profit': [10, 3, 9, 4, 8],
-                                   'size': [14, 4, 10, 6, 9],
-                                   'capacity': 20})
+        out = minizinc(self.model, data=self.data_dict_3)
         self.assertEqual(list(out), [{'x': {3, 5}}])
-
-    def test_minizinc2(self):
-        # test that the temp file is flushed or closed
-        # somehow the file is flushed if there is a \n in the string written
-        out = list(pymzn.minizinc("var 1 .. 1: x; solve maximize x;"))
-        self.assertEqual(list(out), [{'x': 1}])
 
 
 class MinizincTestCalls(unittest.TestCase):
+
     # check that cbc and gecode solver work with diferent combinations
     # of model and data
-    model = '''
-     include "globals.mzn";
-     array[1..5] of var 1 .. 5: x;
-     constraint all_different(x);
-     constraint x[1]>x[2];
-     constraint x[2]>x[3];
-     constraint x[3]>x[4];
-     constraint x[3]>x[5];
-     int: k;
-     constraint x[5] = k;
-     solve maximize x[4]*x[1];
-'''
-    data = {'k':2}
-    solution = [{'x': [5,4,3,1,2]}]
+
+    model = dedent('''\
+        include "globals.mzn";
+
+        array[1 .. 5] of var 1 .. 5: x;
+
+        constraint all_different(x);
+        constraint x[1] > x[2];
+        constraint x[2] > x[3];
+        constraint x[3] > x[4];
+        constraint x[3] > x[5];
+
+        int: k;
+        constraint x[5] = k;
+
+        solve maximize x[4]*x[1];
+    ''')
+
+    data = {'k': 2}
+    solution = [{'x': [5, 4, 3, 1, 2]}]
 
     # test with data as object
     def _test_data_obj(self, solver):
-        out = list(pymzn.minizinc(solver=solver, mzn=self.model, data=self.data))
-        self.assertEqual(out, self.solution)
+        out = minizinc(self.model, solver=solver, data=self.data)
+        self.assertEqual(list(out), self.solution)
 
     def test_data_obj_gecode(self):
-        self._test_data_obj('gecode')
+        self._test_data_obj(gecode)
 
     def test_data_obj_cbc(self):
-        self._test_data_obj('cbc')
+        self._test_data_obj(cbc)
 
     # test with data as string
     def _test_data_str(self, solver):
-        out = list(pymzn.minizinc(solver=solver, mzn=self.model, data='k=2;'))
-        self.assertEqual(out, self.solution)
+        out = minizinc(solver=solver, mzn=self.model, data='k=2;')
+        self.assertEqual(list(out), self.solution)
 
     def test_data_str_gecode(self):
-        self._test_data_str('gecode')
+        self._test_data_str(gecode)
 
     def test_data_str_cbc(self):
-        self._test_data_str('cbc')
-
-
-    def save_as_temp_file(self, contents, suffix):
-        import tempfile
-        f = tempfile.NamedTemporaryFile(mode='w', suffix=suffix, delete=False)
-        file_name = f.name
-        f.write(contents)
-        f.close()
-        return file_name
+        self._test_data_str(cbc)
 
     # test with model as file
     def _test_model_file(self, solver):
-        import os
-        fn = self.save_as_temp_file(self.model +'\nk=2;', '.mzn')
-        out = list(pymzn.minizinc(solver=solver, mzn=fn))
-        os.remove(fn)
-        self.assertEqual(out, self.solution)
+        mzn_file = _save_as_temp_file(self.model + '\nk=2;', '.mzn')
+        out = minizinc(mzn_file, solver=solver)
+        self.assertEqual(list(out), self.solution)
+        os.remove(mzn_file)
 
     def test_model_file_gecode(self):
-        self._test_model_file('gecode')
+        self._test_model_file(gecode)
 
     def test_model_file_cbc(self):
-        self._test_model_file('cbc')
+        self._test_model_file(cbc)
 
     # test with model and data as file
     def _test_model_data_file(self, solver):
-        import os
-        fn = self.save_as_temp_file(self.model, '.mzn')
-        dn = self.save_as_temp_file('k=2;', '.dzn')
-        out = list(pymzn.minizinc(fn, dn, solver=solver))
-        os.remove(fn)
-        self.assertEqual(out, self.solution)
+        mzn_file = _save_as_temp_file(self.model, '.mzn')
+        dzn_file = _save_as_temp_file('k=2;', '.dzn')
+        out = minizinc(mzn_file, dzn_file, solver=solver)
+        self.assertEqual(list(out), self.solution)
+        os.remove(mzn_file)
+        os.remove(dzn_file)
 
     def test_model_data_file_gecode(self):
-        self._test_model_data_file('gecode')
+        self._test_model_data_file(gecode)
 
     def test_model_data_file_cbc(self):
-        self._test_model_data_file('cbc')
+        self._test_model_data_file(cbc)
 
 
 class MinizincTestAllSolutions(unittest.TestCase):
+
     # check that gecode solver returns all solutions
     # this does not work with cbc, we get:
-    #  WARNING. --all-solutions for SAT problems not implemented.
+    # WARNING. --all-solutions for SAT problems not implemented.
     # we get only one solution in that case
-    model  = '''
-     include "globals.mzn";
-     array[1..5] of var 1 .. 5: x;
-     constraint all_different(x);
-     solve satisfy;
-'''
+
+    model  = dedent('''
+        include "globals.mzn";
+        array[1 .. 5] of var 1 .. 5: x;
+        constraint all_different(x);
+        solve satisfy;
+    ''')
+
     def test_gecode(self):
-        out = list(pymzn.minizinc(solver='gecode', mzn=self.model, all_solutions=True))
+        out = minizinc(self.model, solver=gecode, all_solutions=True)
         self.assertEqual(len(out), 120)
+
