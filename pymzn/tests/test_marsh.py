165ad7c4184f453c32c909bf892c91356bedb6ea
Paolo Dragone <dragone.paolo@gmail.com>
2019-03-07 11:29:11 +0100

    tests: refactor test marsh and add test for enums

--- pymzn/tests/test_marsh.py
+++ pymzn/tests/test_marsh.py
@@ -2,6 +2,8 @@ import re
 import pymzn
 import unittest
 
+from enum import Enum
+
 
 class MarshTest(unittest.TestCase):
 
@@ -26,17 +28,25 @@ class MarshTest(unittest.TestCase):
         self.assertEqual(_dzn_value([]), 'array1d({}, [])')
         self.assertEqual(_dzn_value([1, 3]), 'array1d(1..2, [1, 3])')
         self.assertEqual(_dzn_value({1: 1, 2: 3}), 'array1d(1..2, [1, 3])')
-        self.assertEqual(_dzn_value([[1, 2], [3, 4]]),
-                         'array2d(1..2, 1..2, [1, 2, 3, 4])')
-        self.assertEqual(_dzn_value([[1, 2, 3], [4, 5, 6]]),
-                         'array2d(1..2, 1..3, [1, 2, 3, 4, 5, 6])')
-        self.assertEqual(_dzn_value({1: [1, 2, 3], 2: [4, 5, 6]}),
-                         'array2d(1..2, 1..3, [1, 2, 3, 4, 5, 6])')
-        self.assertEqual(_dzn_value({1: {1: 1, 2: 2, 3: 3}, 2: [4, 5, 6]}),
-                         'array2d(1..2, 1..3, [1, 2, 3, 4, 5, 6])')
-        self.assertEqual(_dzn_value({1: {1: 1, 2: 2, 3: 3},
-                                     2: {1: 4, 2: 5, 3: 6}}),
-                         'array2d(1..2, 1..3, [1, 2, 3, 4, 5, 6])')
+        self.assertEqual(
+            _dzn_value([[1, 2], [3, 4]]), 'array2d(1..2, 1..2, [1, 2, 3, 4])'
+        )
+        self.assertEqual(
+            _dzn_value([[1, 2, 3], [4, 5, 6]]),
+            'array2d(1..2, 1..3, [1, 2, 3, 4, 5, 6])'
+        )
+        self.assertEqual(
+            _dzn_value({1: [1, 2, 3], 2: [4, 5, 6]}),
+            'array2d(1..2, 1..3, [1, 2, 3, 4, 5, 6])'
+        )
+        self.assertEqual(
+            _dzn_value({1: {1: 1, 2: 2, 3: 3}, 2: [4, 5, 6]}),
+            'array2d(1..2, 1..3, [1, 2, 3, 4, 5, 6])'
+        )
+        self.assertEqual(
+            _dzn_value({1: {1: 1, 2: 2, 3: 3}, 2: {1: 4, 2: 5, 3: 6}}),
+            'array2d(1..2, 1..3, [1, 2, 3, 4, 5, 6])'
+        )
 
     def test_raises_dzn_value(self):
         class _Dummy(object):
@@ -53,4 +63,16 @@ class MarshTest(unittest.TestCase):
 
     def test_rebase_array(self):
         self.assertEqual(pymzn.rebase_array({2: 1, 3: 2, 4: 3}), [1, 2, 3])
+
+    def test_dzn_enum(self):
+        E = Enum('E', {'A': 1, 'B': 2, 'C': 3})
+
+        self.assertEqual(pymzn.stmt2enum(E), 'enum E = {A,B,C};')
+        self.assertEqual(pymzn.stmt2enum(E, declare=False), 'E = {A,B,C};')
+        self.assertEqual(pymzn.stmt2enum(E, assign=False), 'enum E;')
+
+        # Declare, assign or both, otherwise ValueError is raised
+        self.assertRaises(
+            ValueError, pymzn.stmt2enum, E, declare=False, assign=False
+        )
 
